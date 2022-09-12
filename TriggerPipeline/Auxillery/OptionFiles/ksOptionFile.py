92ceb65a0936877eb897c879ae93ba5d3845cf91
Hashmi <sabinhashmi@gmail.com>
2022-09-12 12:31:40 +0200

    Some results and plots been added

---
+++ TriggerPipeline/Auxillery/OptionFiles/ksOptionFile.py
@@ -0,0 +1,180 @@
+###############################################################################
+# (c) Copyright 2019 CERN for the benefit of the LHCb Collaboration           #
+#                                                                             #
+# This software is distributed under the terms of the GNU General Public      #
+# Licence version 3 (GPL Version 3), copied verbatim in the file "COPYING".   #
+#                                                                             #
+# In applying this licence, CERN does not waive the privileges and immunities #
+# granted to it by virtue of its status as an Intergovernmental Organization  #
+# or submit itself to any jurisdiction.                                       #
+###############################################################################
+"""Options for running HLT2 lines.
+
+Run like any other options file:
+
+    ./Moore/run gaudirun.py hlt2_example.py
+"""
+from Moore import options, run_moore
+
+#from Hlt1Conf.lines.high_pt_muon import all_lines
+#from RecoConf.global_tools import stateProvider_with_simplified_geom
+# TODO stateProvider_with_simplified_geom must go away from option files
+
+input_files = [
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000001_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000002_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000003_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000004_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000005_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000006_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000007_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000008_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000009_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000010_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000011_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000012_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000013_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000014_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000015_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000016_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000017_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000018_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000019_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000020_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000021_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000022_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000023_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000024_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000025_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000026_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000027_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000028_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000029_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000030_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000031_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000032_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000033_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000034_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000035_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000036_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000037_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000038_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000039_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000040_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000041_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000042_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000043_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000044_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000045_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000046_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000047_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000048_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000049_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000051_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000052_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000053_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000054_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000055_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000056_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000057_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000058_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000059_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000060_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000061_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000062_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000063_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000064_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000065_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000066_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000067_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000068_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000069_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000070_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000071_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000072_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000073_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000074_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000075_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000076_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000077_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000078_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000079_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000080_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000081_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000082_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000083_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000084_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000085_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000086_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000087_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000088_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000089_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000090_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000091_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000092_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000093_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000094_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000095_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000096_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000097_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000098_2.xdst',
+'/data2/tracking/trackingGG/frozen_build/data/ks/00104521_00000099_2.xdst',
+]
+
+options.input_files = input_files
+options.input_type = 'ROOT'
+options.input_raw_format = 4.2
+# When running from Upgrade MC, must use the post-juggling locations of the raw
+# event
+
+# options.evt_max = 10000
+options.evt_max = 1
+options.simulation = True
+options.data_type = 'Upgrade'
+options.dddb_tag = 'dddb-20190223'
+options.conddb_tag = 'sim-20180530-vc-md100'
+#options.print_freq = 1
+
+from RecoConf.hlt1_tracking import default_ft_decoding_version
+default_ft_decoding_version.global_bind(value=6)
+
+#from Configurables import VPClus, VeloClusterTrackingSIMD, DigiConf, FTRawBankDecoder
+#VPClus("VPClustering").RawEventLocation = "/Event/Velo/RawEvent"
+#VeloClusterTrackingSIMD("VeloClusterTracking").RawEventLocation = "/Event/Velo/RawEvent"
+#FTRawBankDecoder("createFtClusters").DecodingVersion = 6
+
+#DigiConf().EnableUnpack = True
+
+
+#def make_lines():
+#    return [builder() for builder in all_lines.values()]
+
+
+#public_tools = [stateProvider_with_simplified_geom()]
+#run_reconstruction(options, make_lines, public_tools)
+#run_moore(options, make_lines, public_tools)
+
+
+
+###############################################################################
+# (c) Copyright 2019 CERN for the benefit of the LHCb Collaboration           #
+#                                                                             #
+# This software is distributed under the terms of the GNU General Public      #
+# Licence version 3 (GPL Version 3), copied verbatim in the file "COPYING".   #
+#                                                                             #
+# In applying this licence, CERN does not waive the privileges and immunities #
+# granted to it by virtue of its status as an Intergovernmental Organization  #
+# or submit itself to any jurisdiction.                                       #
+###############################################################################
+
+
+from Moore import  run_reconstruction
+from RecoConf.standalone import *
+from RecoConf.hlt2_tracking import make_hlt2_tracks
+from PyConf.Algorithms import PrLongLivedTracking
+rec = standalone_hlt2_full_track_reco
+
+
+with rec.bind(do_mc_checking=True):
+	with make_hlt2_tracks.bind(fast_reco=True):
+		run_reconstruction(options, rec)
